636654ae3570ebfc6bd12119b867d9e981059081
cweslley <c.yellsew@hotmail.com>
2013-01-29 17:11:17 -0300

    commit projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Amigo::Application.config.secret_token = '9bbf10896fdabf119e9d0cc34e50cdc2b5e53ccba7556005c94bc47ab0edb89607a3ac19b5d3fa82c22098310bc710b01625ad62b7368f8323fc48263b897e95'
